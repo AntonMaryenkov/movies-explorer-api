5a6fcdb5f990907e8f212b332015ebc8bab7397b
Anton Maryenkov <antonmaryenkov@gmail.com>
2021-03-18 18:23:00 +0700

    изменил PORT

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const errorHandler = require('./errors/errorHandler');
 const limiter = require('./middlewares/limiter');
 
-const { PORT = 3001, MONGO_URL = 'mongodb://localhost:27017/moviesdb' } = process.env;
+const { PORT = 3000, MONGO_URL = 'mongodb://localhost:27017/moviesdb' } = process.env;
 const app = express();
 mongoose.connect(MONGO_URL, {
   useNewUrlParser: true,
